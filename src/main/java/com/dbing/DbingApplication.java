03096eaeec3373b9a8e7b38313ffa5e97b796142
engioneer <engioneer@gmail.com>
2022-05-28 19:05:22 +0900

    initial commit

---
+++ src/main/java/com/dbing/DbingApplication.java
@@ -0,0 +1,13 @@
+package com.dbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbing/DbingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
